41bc25121aeff6b31fd85bd7c0f3a98861bc46e8
Stas <privetbrest@gmail.com>
2022-08-18 07:08:05 +0300

    first commit

---
+++ First_shop/views/site/about.php
@@ -0,0 +1,20 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+<section>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-6">
+                <h4>Информация о магазине</h4>
+
+                <br/>
+
+                <p>Лучший магазин в мире </p>
+
+                <p>Это самы качественный сервис, который только может быть</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
